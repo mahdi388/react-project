693467e672fd9736513574f7844c456d0b90e0ed
mahdi388 <ahangarym34@yahoo.com>
2023-09-23 22:11:18 +0330

    add footer

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,5 +1,6 @@
 import "./styles/main.scss"
 import { useState } from "react";
+import ad1 from "./images/ad1.jpg"
 
 function App() {
   const [user,setUser]=useState(null)
@@ -16,6 +17,51 @@ function App() {
         </ul>
       </nav>
     </header>
+    <main>
+    </main>
+      Lorem ipsum dolor sit amet consectetur, <br/>
+      adipisicing elit. Temporibus tenetur <br/>
+      vitae mollitia expedita voluptate nam <br/>
+      deleniti esse porro, quam nisi asperiores <br/>
+      voluptas itaque, vero impedit sunt cum <br/>
+      nemo suscipit laudantium.<br/>
+      Lorem ipsum dolor sit amet consectetur, <br/>
+      adipisicing elit. Temporibus tenetur <br/>
+      vitae mollitia expedita voluptate nam <br/>
+      deleniti esse porro, quam nisi asperiores <br/>
+      voluptas itaque, vero impedit sunt cum <br/>
+      nemo suscipit laudantium.<br/>
+      Lorem ipsum dolor sit amet consectetur, <br/>
+      adipisicing elit. Temporibus tenetur <br/>
+      vitae mollitia expedita voluptate nam <br/>
+      deleniti esse porro, quam nisi asperiores <br/>
+      voluptas itaque, vero impedit sunt cum <br/>
+      nemo suscipit laudantium.<br/>
+      Lorem ipsum dolor sit amet consectetur, <br/>
+      adipisicing elit. Temporibus tenetur <br/>
+      vitae mollitia expedita voluptate nam <br/>
+      deleniti esse porro, quam nisi asperiores <br/>
+      deleniti esse porro, quam nisi asperiores <br/>
+    <footer>
+      <div className="concat-us">
+        <div>Contact us</div>
+        <ul>
+          <li>Address: Hamedan Iran</li>
+          <li>Emali: aria@hotel.com</li>
+          <li>Phone: 09181111111</li>
+        </ul>
+      </div>
+      <div className="adds">
+        <div>
+          <img src={ad1} alt="add"/>
+          <div>Lorem, ipsum.</div>
+        </div>
+        <div>
+          <img src={ad1} alt="add"/>
+          <div>Lorem, ipsum.</div>
+        </div>
+      </div>
+    </footer>
   </>;
 }
 
